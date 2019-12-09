33e220c2ec669eed7f62ae7a164b1d57fd8c2c1f
alannarisse <angrypirate@gmail.com>
2019-12-08 21:28:20 -0800

    added events page

---
+++ js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
