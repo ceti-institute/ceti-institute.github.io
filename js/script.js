3483d062f3f671306eb4a8c451680509850e22b8
alannarisse <angrypirate@gmail.com>
2020-01-02 22:29:01 -0800

    added background image and restyled homepage.

--- js/script.js
+++ js/script.js
@@ -14,7 +14,26 @@ function closeMobileMenu(){
     })
 }
 
+$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
 
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});
 
 //when the page loads call toggleIcon;
 $(toggleIcon);
