438bc85902bd01e0d306bf3e362bb4ac46a77698
alannarisse <angrypirate@gmail.com>
2020-01-06 00:56:43 -0800

    added people

---
+++ js/people.js
@@ -0,0 +1,256 @@
+const PEOPLE_DATA = [
+		{
+			name: 'Laura Alcorn',
+			img: 'laura_allcorn_headshot.jpg',
+			page: 'alanna-risse.html',
+			link: 'https://alannarisse.com'
+		},
+		{
+			name: 'Sara Bernstein, Ph.D.',
+			img: 'sara-bernstein.jpg',
+			page: 'scott-garner.html',
+			link: 'https://scottgarner.com'
+		},
+    {
+      name: 'Simon Boas',
+      img: 'simonboas_photo.jpg',
+      page: 'simon-boas.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Reese Bowes',
+      img: 'Reese-Bowes.png',
+      page: 'resse-bowes.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'John Brown',
+      img: 'John_Brown.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Maxwell Brown',
+      img: 'Maxwell-Brown.jpeg',
+      page: 'maxwell-brown.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Surya Buchwald',
+      img: 'Surya_buchwald.png',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Jesse England',
+      img: 'Jesse_England.jpg',
+      page: 'jesse_england.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Aubrey Francois',
+      img: 'aubreyfrancois_headshot.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Scott Garner',
+      img: 'Scott-Garner.jpeg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Mike Heavers',
+      img: 'Mike_Heavers.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Tiffany Henschel',
+      img: 'Tiffany-Henschel.jpg',
+      page: 'tiffany-henschel.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Anselm Hook',
+      img: 'anselm.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Yori Kvitchko',
+      img: 'Yori-Kvitchko.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Zach Marlow-McCarthy',
+      img: 'Zack_Marlow-McCarthy.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Stephanie Mendoza',
+      img: 'stephanie-mendoza.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Daniel Meyers',
+      img: 'daniel_meyers.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Darcy Neal',
+      img: 'Darcy_Neal_headshot.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Salvador Orara',
+      img: 'Sal-Orara.jpg',
+      page: 'scott-garner.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Hugo Paris',
+      img: 'person-generic.png',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Justin Patrizi',
+      img: 'person-generic.png',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Andy Powell',
+      img: 'andy-powell-superfab.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Ben Purdy',
+      img: 'benpurdy.png',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Nandini Ranganathan',
+      img: 'nandini-ranganathan.jpg',
+      page: 'people.html',
+      link: 'https://link.com'
+    },
+    {
+      name: 'Matthew Rhoades',
+      img: 'Matt_Rhoades.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Jeremy Rotsztain',
+      img: 'jeremyrotsztain.jpg',
+      page: 'people.html',
+      link: 'https://link.com'
+    },
+    {
+      name: 'Alice Rotsztain',
+      img: 'Alice-Rotsztain.jpg',
+      page: 'people.html',
+      link: 'https://link.com'
+    },
+    {
+      name: 'David Sherwin',
+      img: 'David_at_Lazarid.jpg',
+      page: 'people.html',
+      link: 'https://link.com'
+    },
+    {
+      name: 'Mary Paynter Sherwin',
+      img: 'marysherwin_headshot.jpg',
+      page: 'people.html',
+      link: 'https://link.com'
+    },
+    {
+      name: 'Howard Silverman',
+      img: 'howard_silverman.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Traci Sym',
+      img: 'traciSym.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Peter Vickery',
+      img: 'Peter-Vickery.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Daniel Wenger',
+      img: 'Daniel_Wenger_Headshot.jpeg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Thomas Wester',
+      img: 'Thomas-Wester.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Shannon Willis',
+      img: 'shannon_willis.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Craig Winslow',
+      img: 'Craig-Winslow-updated.jpg',
+      page: 'people.html',
+      link: 'https://scottgarner.com'
+    },
+    {
+      name: 'Alanna Risse',
+      img: 'alanna-risse.png',
+      page: 'alanna-risse.html',
+      link: 'https://alannarisse.com'
+    }
+	];
+
+function renderPeople(array) {
+  console.log(PEOPLE_DATA.length);
+  let htmlfun = ''
+  for (let i = 0; i < PEOPLE_DATA.length; i++ ){
+
+    $('main').find('#peopleGrid').append(`
+    <div class="person">
+        <img src="images/people/${PEOPLE_DATA[i].img}" alt="${PEOPLE_DATA[i].name}">
+        <div class="overlay">
+          <div class="text">${PEOPLE_DATA[i].name}</div>
+        </div>
+      </div>
+      `);
+  }
+  return htmlfun;
+
+
+}
+
+
+function addPeopleHtml() {
+  $('main').find('#peopleGrid').append(renderPeople(PEOPLE_DATA));
+}
+
+
+
+function init() {
+  renderPeople(PEOPLE_DATA);
+}
+
+$(init);
