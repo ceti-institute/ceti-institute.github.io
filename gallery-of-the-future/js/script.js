6c87c2dbb75594cbeb4081532b8c13fa188ab16e
alannarisse <angrypirate@gmail.com>
2020-07-23 09:28:08 -0700

    added gof page

---
+++ gallery-of-the-future/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$('.popup-with-form').magnificPopup({
+		type: 'inline',
+		preloader: false,
+		focus: '#name',
+
+		// When elemened is focused, some mobile browsers in some cases zoom in
+		// It looks not nice, so we disable it:
+		callbacks: {
+			beforeOpen: function() {
+
+				if($(window).width() < 700) {
+					this.st.focus = false;
+				} else {
+					this.st.focus = '#name';
+				}
+			}
+		}
+	});
+});
