9a45b6c92306e2a5ae88f13b2febb0916d37fe58
alannarisse <angrypirate@gmail.com>
2022-01-22 16:28:15 -0800

    added new participants

--- programs/constellations/participants.js
+++ programs/constellations/participants.js
@@ -1,79 +1,82 @@
 //participants from https://docs.google.com/spreadsheets/d/1diYFaKSkEImWN75mjc4ihQ6KActnkWCxMdlz4Vfzahc/edit#gid=1812818967
 DATA = [
-  { first: "Alain", last: "Bally", email: "alain.bally@gmail.com", web: "", linky: "" },
+  { first: "Alain", last: "Bally", web: "", linky: "" },
 
-  { first: "Alanna", last: "Risse", email: "arisse@pdx.edu", web: "@alannarisse", linky: "http://alannarisse.com" },
+  { first: "Alanna", last: "Risse", web: "@alannarisse", linky: "http://alannarisse.com" },
 
-  { first: "alex", last: "michaluk", email: "orangeskeleton@gmail.com", web: "", linky: "" },
+  { first: "alex", last: "michaluk", web: "", linky: "" },
 
-  { first: "Allison", last: "Meechoovet", email: "ameechoovet@gmail.com", web: "", linky: "" },
+  { first: "Allison", last: "Meechoovet", web: "", linky: "" },
 
-  { first: "Anand", last: "Kailasam", email: "anand@pdx.edu", web: "https://www.behance.net/ArkReactor", linky: "https://www.behance.net/ArkReactor" },
+  { first: "Anand", last: "Kailasam", web: "https://www.behance.net/ArkReactor", linky: "https://www.behance.net/ArkReactor" },
 
-  { first: "BB", last: "Andersson", email: "b.anderson@pdx.edu", web: "bbandersson.com/", linky: "http://bbandersson.com/" },
+  { first: "BB", last: "Andersson", web: "bbandersson.com/", linky: "http://bbandersson.com/" },
   {
     first: "Benjamin",
     last: "Hencke",
-    email: "hencke@gmail.com",
     web: "bhencke.com",
     linky: "http://bhencke.com"
   },
 
-  { first: "Bernardo", last: "Pantoja", email: "bernazombie@gmail.com", web: "https://vimeo.com/hologramfields", linky: "https://vimeo.com/hologramfields" },
+  { first: "Bernardo", last: "Pantoja", web: "https://vimeo.com/hologramfields", linky: "https://vimeo.com/hologramfields" },
 
-  { first: "Brendan", last: "Jones", email: "brendanjones.pdxdfv@gmail.com", web: "", linky: "" },
+  { first: "Brendan", last: "Jones", web: "", linky: "" },
 
-  { first: "DEEPAK", last: "KELOTH", email: "deepak.keloth@gmail.com", web: "", linky: "" },
+  { first: "DEEPAK", last: "KELOTH", web: "", linky: "" },
+  { first: "Byron", last: "Greenlee", web: "", linky: "" },
+  { first: "Elaina", last: "Tardif", web: "", linky: "" },
+  { first: "Ola", last: "Onipede", web: "", linky: "" },
+  { first: "Emmett", last: "Crass", web: "@cosmos_quartz", linky: "http://instagram.com/cosmos_quartz" },
 
-  { first: "Elaina", last: "Tardif", email: "elainatardif@gmail.com", web: "", linky: "" },
+  { first: "Fernanda", last: "D'Agostino", web: "https://www.fernandadagostino.com/", linky: "https://www.fernandadagostino.com/" },
 
-  { first: "Emmett", last: "Crass", email: "ecrass@pdx.edu", web: "@cosmos_quartz", linky: "http://instagram.com/cosmos_quartz" },
+  { first: "Helen", last: "Leigh", web: "twitter: @helenleigh", linky: "https://twitter.com/helenleigh" },
 
-  { first: "Fernanda", last: "D'Agostino", email: "nandada@aol.com", web: "https://www.fernandadagostino.com/", linky: "https://www.fernandadagostino.com/" },
+  { first: "Hilary", last: "Tsai", web: "", linky: "" },
 
-  { first: "Helen", last: "Leigh", email: "helen.leigh@crowdsupply.com", web: "twitter: @helenleigh", linky: "https://twitter.com/helenleigh" },
+  { first: "Jacob", last: "Richman", web: "", linky: "" },
 
-  { first: "Hilary", last: "Tsai", email: "htsai@pnca.edu", web: "", linky: "" },
+  { first: "Zack", last: "Marlow", web: "zackmarlow.com", linky: "http://www.zackmarlow.com/" },
 
-  { first: "Jacob", last: "Richman", email: "richman.jacob@gmail.com", web: "", linky: "" },
+  { first: "Loni", last: "Hoodenpyle", web: "", linky: "" },
 
-  { first: "Jerry", last: "Belich", email: "jerry@jerrytron.com", web: "https://jerrytron.com", linky: "https://jerrytron.com" },
+  { first: "Jerry", last: "Belich", web: "https://jerrytron.com", linky: "https://jerrytron.com" },
 
-  { first: "Jesse", last: "England", email: "jesse.england@gmail.com", web: "http://jesseengland.net", linky: "http://jesseengland.net" },
+  { first: "Jesse", last: "England", web: "http://jesseengland.net", linky: "http://jesseengland.net" },
 
-  { first: "JJ", last: "Vazquez", email: "jjv4@pdx.edu", web: "www.locomotionshoots.com", linky: "http://www.locomotionshoots.com" },
+  { first: "JJ", last: "Vazquez", web: "www.locomotionshoots.com", linky: "http://www.locomotionshoots.com" },
 
-  { first: "Julie", last: "Perini", email: "perini@pdx.edu", web: "www.julieperini.com", linky: "http://www.julieperini.com" },
+  { first: "Julie", last: "Perini", web: "www.julieperini.com", linky: "http://www.julieperini.com" },
+  { first: "David", last: "Lobser", web: "http://dlobser.com", linky: "http://dlobser.com" },
+  { first: "Justin", last: "Patrizi", web: "", linky: "" },
+  { first: "Layna", last: "Lewis", web: "@elevengallonhead https://www.instagram.com/vivalafreepdx/", linky: "https://www.instagram.com/vivalafreepdx/" },
+  { first: "Georgia ", last: "Bryant", web: "", linky: "" },
+  { first: "Karl", last: "Benjamin", web: "", linky: "" },
+  { first: "Leslie ", last: "Villela", web: "", linky: "" },
+  { first: "Laurel ", last: "Jensen", web: "", linky: "" },
+  { first: "Lillyanne", last: "Pham", web: "lillyannepham.com / instagram: @lillyannepham", linky: "http://instagram.com/lillyannepham" },
 
-  { first: "Justin", last: "Patrizi", email: "justinzero@gmail.com", web: "", linky: "" },
+  { first: "Matthew", last: "Sottile", web: "", linky: "" },
+  { first: "Brad", last: "Andersen", web: "", linky: "" },
+  { first: "Ayse", last: "Gursoz", web: "aysegursoz.com", linky: "http://www.aysegursoz.com" },
+  { first: "Keenan", last: "McCune", web: "@symmetriccollective", linky: "https://www.instagram.com/symmetriccollective/" },
+  { first: "Michelle", last: "Guthrie", web: "@Michellemguthrie on IG", linky: "http://instagram.com/Michellemguthrie" },
 
-  { first: "Layna", last: "Lewis", email: "llewis@willamette.edu", web: "@elevengallonhead https://www.instagram.com/vivalafreepdx/", linky: "https://www.instagram.com/vivalafreepdx/" },
+  { first: "Miguel", last: "Higgins Moy", web: "", linky: "" },
 
-  { first: "Leslie ", last: "Villela", email: "leslie.villela@intel.com", web: "", linky: "" },
+  { first: "Nandini", last: "Ranganathan", web: "", linky: "" },
 
-  { first: "Lillyanne", last: "Pham", email: "lilpham@pdx.edu", web: "lillyannepham.com / instagram: @lillyannepham", linky: "http://instagram.com/lillyannepham" },
+  { first: "Peter", last: "Vickery", web: "", linky: "" },
 
-  { first: "Matthew", last: "Sottile", email: "mjsottile@me.com", web: "", linky: "" },
+  { first: "Rashad", last: "Pridgen", web: "www.rashadxsoulnubian.com", linky: "http://www.rashadxsoulnubian.com" },
 
-  { first: "Michelle", last: "Guthrie", email: "michelleguthrie@gmail.com", web: "@Michellemguthrie on IG", linky: "http://instagram.com/Michellemguthrie" },
+  { first: "Scott", last: "Nieradka", web: "", linky: "" },
 
-  { first: "Miguel", last: "Higgins Moy", email: "mhigginsmoy@gmail.com", web: "", linky: "" },
+  { first: "Scott", last: "Garner", web: "http://scottmadethis.net/", linky: "http://scottmadethis.net/" },
 
-  { first: "Nandini", last: "Ranganathan", email: "nandini@ceti.institute", web: "", linky: "" },
+  { first: "Shana ", last: "Palmer", web: "ShanaPalmer.com", linky: "http://ShanaPalmer.com" },
 
-  { first: "Peter", last: "Vickery", email: "Vfxpeter@gmail.com", web: "", linky: "" },
-
-  { first: "Rashad", last: "Pridgen", email: "rashadpridgen.ceo@gmail.com", web: "www.rashadxsoulnubian.com", linky: "http://www.rashadxsoulnubian.com" },
-
-  { first: "Scott", last: "Nieradka", email: "nieradka@pdx.edu", web: "", linky: "" },
-
-  { first: "Scott", last: "Garner", email: "scott@j38.net", web: "http://scottmadethis.net/", linky: "http://scottmadethis.net/" },
-
-  { first: "Shana ", last: "Palmer", email: "Beverlyshana@gmail.com", web: "ShanaPalmer.com", linky: "http://ShanaPalmer.com" },
-
-  { first: "Stephen", last: "Lee", email: "Steplee@pdx.edu", web: "@j_stephen_lee, jstephenlee.com", linky: "http://jstephenlee.com" },
-
-  { first: "Tomas", last: "Cotik", email: "tcotik@pdx.edu", web: "", linky: "" }
+  { first: "Tomas", last: "Cotik", web: "", linky: "" }
 ]
 
 function createParticipant(DATA) {
